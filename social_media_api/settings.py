f2e98b24597c6ffba4f7ba265d39a94698b548f8
@Mikist <mikeochango3@gmail.com>
2025-01-08 17:41:33 +0300

    configure to use sql  databsae

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -78,13 +78,26 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media_api',
+        'USER': 'root',
+        'PASSWORD': '@Mikist@23.@Mikist@23.',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
