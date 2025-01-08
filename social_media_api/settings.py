1df296ad56a738dce15de5fc790ac87069e54d1b
@Mikist <mikeochango3@gmail.com>
2025-01-08 17:46:26 +0300

    urls in project coonfiguration

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -162,4 +162,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
