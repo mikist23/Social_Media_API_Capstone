8dbff7d620ac2c56f22ee65efccf80351660d388
@Mikist <mikeochango3@gmail.com>
2025-01-08 17:43:09 +0300

    set to use the custom auithentication models

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -117,6 +117,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
