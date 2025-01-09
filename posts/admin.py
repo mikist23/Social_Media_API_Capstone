d84efa6d16c32f7a8be10d37188a55a9ffcf0d84
@Mikist <mikeochango3@gmail.com>
2025-01-09 16:25:55 +0300

    Add authentications and admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import  Post, Comment
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']
+    search_fields = ['title', 'content']
+    list_filter = ['created_at', 'updated_at']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'post', 'created_at']
+    search_fields = ['content']
+    list_filter = ['created_at']
