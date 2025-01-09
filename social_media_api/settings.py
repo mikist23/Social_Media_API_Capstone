03190afdfe59d3f53b06287c8089e7e99c3f561b
@Mikist <mikeochango3@gmail.com>
2025-01-09 16:22:57 +0300

    Add posts app

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'posts',
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -124,7 +125,7 @@ AUTHENTICATION_BACKENDS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'accounts.authentication.BearerTokenAuthentication', # Use your custom class
-        # 'posts.authentication.BearerTokenAuthentication', 
+        'posts.authentication.BearerTokenAuthentication', 
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
