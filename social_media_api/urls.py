c173ce6781ae0ed025c6f484ee9ae2b8adc3e7f0
@Mikist <mikeochango3@gmail.com>
2025-01-11 12:11:08 +0300

    to include swagger documentation

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -1,25 +1,27 @@
-"""
-URL configuration for social_media_api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Define schema view for API documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Social Media API",
+        default_version='v1',
+        description="API documentation for the Social Media API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@socialmediaapi.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("accounts.urls")),
     path('api/', include('posts.urls')),
     path('api/', include('notifications.urls')),
+    path('swagger/', schema_view.as_view(), name='schema-swagger-ui'),  # Swagger UI for API docs
 ]
