639d81036dcc8f5a4005cd3507778ee27ba18b78
@Mikist <mikeochango3@gmail.com>
2025-01-23 19:07:17 +0300

    # Swagger UI for API docs

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include("accounts.urls")),
     path('api/', include('posts.urls')),
     path('api/', include('notifications.urls')),
-    path('swagger/', schema_view.as_view(), name='schema-swagger-ui'),  # Swagger UI for API docs
+    path('swagger/', schema_view.as_view(), name='schema-swagger-ui'), 
+     # Swagger UI for API docs
 ]
 
